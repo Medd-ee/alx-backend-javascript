a0eb54b1397c8ce1472839a326fd37d30756c971
root <odungideemediong@gmail.com>
2023-06-27 19:48:02 +0000

    ES6 promise

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,14 @@
+0-promise.js
+export function uploadPhoto() {
+  return Promise.resolve({
+    status: 200,
+    body: 'photo-profile-1',
+  });
+}
+
+export function createUser() {
+  return Promise.resolve({
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+  });
+}
